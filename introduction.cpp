08df200cddcc6f13eacf596d73cf0344c2b82a34
Big Bill <60592559+sashascul@users.noreply.github.com>
2022-09-12 15:38:20 +0300

    Add files via upload

---
+++ introduction.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int main () {
+
+    /*
+
+    using namespace std;
+    cout << "Come up and C++ me some time.";
+    cout << endl;
+    cout << "You won't regret it!";
+
+    cin.get (); // окно остаётся открытым, пока не нажата клавиша
+
+    */
+
+    /*
+
+    using std :: cin; // разрешение на использование cin из пространства std;
+    using std :: cout; // разрешение на использование cout из пространства std;
+
+    int a;
+    cin >> a;
+    cout << a;
+
+    */
+
+    /*
+
+    int a;
+    std :: cin >> a;  // использование cin из пространства более ущербным способом)))
+    std :: cout << a; // также и с cout;
+
+    */
+
+    return 0;
+
+}
